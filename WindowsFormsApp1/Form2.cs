1f7b818ec677147387503ec56d00be5287b460ba
alex <adzulaj@gmail.com>
2021-01-22 15:46:34 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+			//Создание объекта для генерации чисел
+			Random rnd = new Random();
+
+			//Получить случайное число (в диапазоне от 0 до 10)
+			int count = rnd.Next(1, 3);
+			 //тут я увеличиваю значения счетчика на 1
+			if (count == 1)
+			{
+				//если счетчик равен 1 то медведь
+				pictureBox1.Image = Properties.Resources.i3;
+
+			}
+			if (count == 2)
+			{
+				//если счетчик равен 2 то конь
+				pictureBox1.Image = Properties.Resources.i2;
+			}
+			if (count == 3)
+			{
+				//если счетчик равен 3 то щенок и котенок
+				pictureBox1.Image = Properties.Resources.i1;
+			}
+			
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Environment.Exit(0);
+		}
+    }
+}
---
+++ WindowsFormsApp1/Form3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form5 f2 = new Form5();
+
+            f2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form5 f2 = new Form5();
+
+            f2.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form4 f2 = new Form4();
+
+            f2.Show();
+        }
+    }
+}
